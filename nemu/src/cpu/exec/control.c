cadcefd8ed2fbf093060a371d2de8552686c4a17
tracer-ics2017 <tracer@njuics.org>
2023-04-17 23:46:22 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  23:46:22 up  1:00,  2 users,  load average: 1.66, 1.36, 1.30 d4c87260c8585ee759d51e9ed5cf05abb7ec9ee

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,7 +25,7 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  rtl_push(&cpu.eip);
+  rtl_push(&cpu.eip+4);
   decoding.is_jmp=1;
 
   print_asm("call %x", decoding.jmp_eip);
