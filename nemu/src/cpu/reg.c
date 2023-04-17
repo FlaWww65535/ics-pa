c8bfaa7b172a9cbe8abd394edb8b112cc8657387
tracer-ics2017 <tracer@njuics.org>
2023-04-17 15:06:35 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  15:06:35 up  1:04,  2 users,  load average: 1.36, 0.91, 0.77 b19b719ace6ec30092c0a9be520ace29f5cf4172

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -35,11 +35,11 @@ void reg_test() {
   assert(reg_b(R_DL) == (sample[R_EDX] & 0xff));
   assert(reg_b(R_DH) == ((sample[R_EDX] >> 8) & 0xff));
 
-  assert(cpu.OF == (sample[R_EFLAGS]&(1<<11)));
-  // assert(cpu.IF == (sample[R_EFLAGS]&(1<<9)));
-  // assert(cpu.SF == (sample[R_EFLAGS]&(1<<7)));
-  // assert(cpu.ZF == (sample[R_EFLAGS]&(1<<6)));
-  // assert(cpu.CF == (sample[R_EFLAGS]&(1<<0)));
+  assert((cpu.OF<<11) == (sample[R_EFLAGS]&(1<<11)));
+  assert((cpu.IF<<11) == (sample[R_EFLAGS]&(1<<9)));
+  assert((cpu.SF<<11) == (sample[R_EFLAGS]&(1<<7)));
+  assert((cpu.ZF<<11) == (sample[R_EFLAGS]&(1<<6)));
+  assert((cpu.CF<<11) == (sample[R_EFLAGS]&(1<<0)));
 
   assert(sample[R_EAX] == cpu.eax);
   assert(sample[R_ECX] == cpu.ecx);
