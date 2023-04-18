f3a41945b005038027e114bfe051366a7573d8b9
tracer-ics2017 <tracer@njuics.org>
2023-04-18 09:31:23 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  09:31:23 up  1:24,  2 users,  load average: 0.66, 0.85, 0.80 97d461b4800f5d1c2f19ecd7fa1b48cd853c8787

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -13,15 +13,36 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
+  uint32_t cf;
+  rtl_get_CF(&cf);
+  uint32_t zf;
+  rtl_get_ZF(&zf);
+  uint32_t of;
+  rtl_get_OF(&of);
+  uint32_t sf;
+  rtl_get_SF(&sf);
   switch (subcode & 0xe) {
     case CC_O:
+    *dest = of;
+    break;
     case CC_B:
+    *dest = cf;
+    break;
     case CC_E:
+    *dest = zf;
+    break;
     case CC_BE:
+    *dest = (cf||zf);
+    break;
     case CC_S:
+    *dest = sf;
+    break;
     case CC_L:
+    *dest = (sf!=of);
+    break;
     case CC_LE:
-      TODO();
+    *dest = (zf||(sf!=of));
+    break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
