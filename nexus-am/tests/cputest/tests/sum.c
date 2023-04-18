16d023d6e1f7097aeba6c18bcf49fc5a3ad38e62
tracer-ics2017 <tracer@njuics.org>
2023-04-18 21:00:07 +0800

    >  run 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  21:00:07 up  4:47,  2 users,  load average: 0.19, 0.52, 0.44 189fdeecf3ab8c499abfc9c8a735fdd1c6aa88fb

--- nexus-am/tests/cputest/tests/sum.c
+++ nexus-am/tests/cputest/tests/sum.c
@@ -3,12 +3,12 @@
 int main() {
 	int i = 1;
 	volatile int sum = 0;
-	while(i <= 100) {
+	while(i <= 1) {
 		sum += i;
 		i ++;
 	}
 
-	nemu_assert(sum == 5050);
+	nemu_assert(sum == 1);
 
 	return 0;
 }
