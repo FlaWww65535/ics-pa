91fa4d6b3342abb262321c43bd1ea14119350010
tracer-ics2017 <tracer@njuics.org>
2023-04-18 09:06:32 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  09:06:32 up 59 min,  2 users,  load average: 0.63, 0.61, 0.67 f3ecf33cece976093e9afa4ef85169dab5cd2532

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,9 +1,9 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov);
+
 make_EHelper(call);
 make_EHelper(push);
-
 make_EHelper(ret);
 
 make_EHelper(add);
@@ -15,9 +15,12 @@ make_EHelper(sub);
 make_EHelper(xor);
 make_EHelper(cmp);
 
+
 make_EHelper(lea);
 
 make_EHelper(operand_size);
+make_EHelper(nop);
 
 make_EHelper(inv);
+
 make_EHelper(nemu_trap);
