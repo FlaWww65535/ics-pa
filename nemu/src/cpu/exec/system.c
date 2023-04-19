84caaf4d96b5d018a481dc6b646c9792981cf34e
tracer-ics2017 <tracer@njuics.org>
2023-04-19 21:04:42 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  21:04:42 up  8:05,  2 users,  load average: 1.47, 1.37, 1.39 4886da26a90db32e8c58aa105d1bf239e844c649

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -45,7 +45,8 @@ uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  pio_read(id_src->addr,id_dest->width);
+  t2 = pio_read(id_src->addr,id_dest->width);
+  operand_write(id_dest,&t2);
 
   print_asm_template2(in);
 
