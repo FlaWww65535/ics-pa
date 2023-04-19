3786ab4c4765a4022a27d34afaf3a4e71bd8ddb1
tracer-ics2017 <tracer@njuics.org>
2023-04-19 18:53:56 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  18:53:56 up  5:54,  2 users,  load average: 2.19, 1.92, 1.70 9b9faf3cd1d55fd1f0469accd2c128431e05926a

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -199,12 +199,9 @@ static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  printf("before%x\n",*dest);
   uint32_t mask=1<<(width*8-1);
-  printf("mask%x\n",mask);
   uint32_t val = (mask&*src1)!=0;
   rtl_mv(dest,&val);
-  printf("after%x\n",*dest);
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
