fb5a230652900b3c17a9b2d3f2a97ca1b8ecb71a
tracer-ics2017 <tracer@njuics.org>
2023-04-19 17:36:22 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  17:36:22 up  4:37,  2 users,  load average: 2.68, 2.32, 2.17 516a7542375ba117b2ee0a8c6a4183618ba06768

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -45,7 +45,7 @@ make_EHelper(sub) {
 make_EHelper(cmp) {
   rtl_sub(&t2, &id_dest->val, &id_src->val);
   rtl_sltu(&t3, &id_dest->val, &t2);
-
+  printf("",id_dest->val);
   rtl_update_ZFSF(&t2, id_dest->width);
 
   rtl_sltu(&t0, &id_dest->val, &t2);
