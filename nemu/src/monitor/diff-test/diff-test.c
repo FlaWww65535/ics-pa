be2e4855446f7e31bdb61c9daf99c897c08ff33f
tracer-ics2017 <tracer@njuics.org>
2023-04-19 12:32:46 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  12:32:46 up 20:20,  2 users,  load average: 1.18, 1.08, 0.84 a21bba54b74946c26528f1913155756e9429df5c

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -83,7 +83,7 @@ void init_difftest(void) {
     if (getppid() != ppid_before_fork) {
       panic("parent has died!");
     }
-
+    printf("test");
     close(STDIN_FILENO);
     execlp("qemu-system-i386", "qemu-system-i386", "-S", "-s", "-nographic", NULL);
     perror("exec");
@@ -91,7 +91,7 @@ void init_difftest(void) {
   }
   else {
     // father
-    return ;
+
     gdb_connect_qemu();
     Log("Connect to QEMU successfully");
 
