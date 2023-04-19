2ba2f7611a469e052ae5b6a5c4fdc550ea3fbdcd
tracer-ics2017 <tracer@njuics.org>
2023-04-19 21:56:13 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  21:56:13 up  8:57,  2 users,  load average: 2.40, 2.09, 1.98 1e6e252e39a692fdaf3d3898c774f920d681794d

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -46,10 +46,15 @@ make_EHelper(or) {
 
 make_EHelper(sar) {
   // unnecessary to update CF and OF in NEMU
-  rtl_mv(&t2,&id_dest->val);
-  rtl_mv(&t3,&id_src->val);
-  rtl_sar(&t0,&t2,&t3);
-  operand_write(id_dest,&t0);
+	if (id_dest->width == 1) {
+		id_dest->val = (int8_t)id_dest->val;
+	}
+	else if (id_dest->width == 2) {
+		id_dest->val = (int16_t)id_dest->val;
+	}
+	rtl_sar(&t2, &id_dest->val, &id_src->val);
+	operand_write(id_dest, &t2);
+	rtl_update_ZFSF(&t2, id_dest->width);
   print_asm_template2(sar);
 }
 
