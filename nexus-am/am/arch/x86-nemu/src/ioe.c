5101a2622308b4ea6b998e187ad3e97a17957eb5
tracer-ics2017 <tracer@njuics.org>
2023-04-26 23:12:26 +0800

    >  run 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  23:12:26 up 46 min,  3 users,  load average: 0.74, 0.68, 0.58 e0baa5eb995e800693460821ca6767486b1f8d80

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -22,15 +22,15 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {  int i;
-  for(int j=0;j<=w;j++){
-  for(int i=0;i<=h;i++){//col
-    //row
-      fb[(y+i)*_screen.width+x+j]=pixels[i*w+j];
-    }
-  }
-  // for (int i=0;i<h;i++){
-  //   memcpy(fb+(y+i)*_screen.width+x,pixels+i*w,w*4);
+  // for(int j=0;j<=w;j++){
+  // for(int i=0;i<=h;i++){//col
+  //   //row
+  //     fb[(y+i)*_screen.width+x+j]=pixels[i*w+j];
+  //   }
   // }
+  for (int i=0;i<h;i++){
+    memcpy(fb+(y+i)*_screen.width+x,pixels+i*w,w*4);
+  }
 }
 
 void _draw_sync() {
