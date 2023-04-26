414d0c8a9cfc7542fa9b54143c9b243698eed752
tracer-ics2017 <tracer@njuics.org>
2023-04-26 18:52:10 +0800

    >  run 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  18:52:10 up 20 min,  2 users,  load average: 0.48, 0.66, 0.53 46b0d6df1ef1204f1bc65e04fa6808415980bf98

---
+++ nexus-am/tests/mytest/main.cpp
@@ -0,0 +1,21 @@
+#include <am.h>
+#include <klib.h>
+#define FPS 32
+
+unsigned int canvas[32][32]={32};
+
+int main() {
+  _ioe_init();
+
+  unsigned long last = 0;
+
+  while (true) {
+    unsigned long upt = _uptime();
+    if (upt - last > 1000 / FPS) {
+      _draw_rect(&canvas[0][0],5,5,32,32);
+      last = upt;
+    }
+  }
+
+  return 0;
+}
