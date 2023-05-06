5e827e498a6fba2b2765e46c04ea9bd37f717fa9
tracer-ics2017 <tracer@njuics.org>
2023-05-06 19:12:59 +0800

    >  run 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  19:12:59 up 41 min,  2 users,  load average: 0.51, 0.83, 0.76 246722838a686c45495ba5a3649af7035937c147

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,8 +3,8 @@
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
+  TODO();
   size_t disk_size = get_ramdisk_size();
   ramdisk_read(DEFAULT_ENTRY,0,disk_size);
-  printf("test");
   return (uintptr_t)DEFAULT_ENTRY;
 }
