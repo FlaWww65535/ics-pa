3a7b1c9fbe62f279f4ad2f29b25e99d931ab9c2d
tracer-ics2017 <tracer@njuics.org>
2023-05-29 21:48:16 +0800

    >  compile 2012235 flaw Linux archos 6.2.11-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 13 Apr 2023 16:59:24 +0000 x86_64 GNU/Linux  21:48:16 up  4:46,  2 users,  load average: 0.95, 0.96, 0.92 69e14e30fb0d5bd573f41d5662956da995294568

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,8 +5,16 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  rtl_push(&cpu.eflags);
+  rtl_push(&cpu.cs);
+  rtl_push(&ret_addr);
 
-  TODO();
+  vaddr_t Gate_addr =cpu.idtr.base+NO*4;
+  int32_t Gate_lo = vaddr_read(Gate_addr,4)&0xffff;  
+  int32_t Gate_hi = vaddr_read(Gate_addr+4,4)&0xffff0000; 
+
+  decoding.jmp_eip=Gate_hi|Gate_lo;
+  decoding.is_jmp=true;
 }
 
 void dev_raise_intr() {
