b43eb719e1d4487157f127bb7d24dacf231cbcd4
tracer-ics2017 <tracer@njuics.org>
2023-05-30 23:39:44 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  23:39:44 up  7:58,  3 users,  load average: 1.42, 1.81, 1.48 858bea818f34589f5091313163fb3daa3e1581

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -2,16 +2,12 @@
 #include <assert.h>
 
 int main() {
-
   FILE *fp = fopen("/share/texts/num", "r+");
   assert(fp);
-  
-  printf("text begin\n");
-  fseek(fp, 0, SEEK_END);
-  long size = fseek(fp,0,SEEK_CUR);
-  printf("size = %d\n",size);
-  assert(size == 5000);
 
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+  assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
@@ -19,28 +15,22 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("text para 2 \n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
-  printf("text para 3 \n");
-
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
-  printf("text para 4 \n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
-  printf("text para 5 \n");
 
   fclose(fp);
 
