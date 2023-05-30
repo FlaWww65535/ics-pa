8bae8a55cbd1ad8f358e62ca92940504147cb95c
tracer-ics2017 <tracer@njuics.org>
2023-05-30 20:27:29 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  20:27:29 up  4:46,  3 users,  load average: 0.49, 0.45, 0.61 bc25f9d82e7421f297e9bd81cef10efad4fe8a3

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,9 +1,9 @@
 #include "fs.h"
-
 typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -24,3 +24,19 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+Finfo f;
+
+int fs_open(const char *pathname){
+  for(int i=0;i<NR_FILES;i++){
+    if(strcmp(file_table[i].name,pathname)==0){
+      f=file_table[i];
+    }
+  }
+}
+ssize_t fs_read(int fd, void *buf, size_t len){
+  ramdisk_read(buf,file_table[fd],len);
+}
+ssize_t fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
+int fs_close(int fd);
