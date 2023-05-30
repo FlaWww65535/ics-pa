a39706024639b00304d77afeb030edc4dcc0aec1
tracer-ics2017 <tracer@njuics.org>
2023-05-30 14:39:25 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  14:39:25 up 39 min,  2 users,  load average: 1.02, 0.77, 0.60 41e250ac890c434e790ed811c0a7234b5cc1145a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,15 @@
 #include "common.h"
 #include "syscall.h"
 
+int sys_none(uint32_t r){
+  return 1;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-
   switch (a[0]) {
-    case 0: return 1;
+    case 0: SYSCALL_ARG1(r)=sys_none(a[0]);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
