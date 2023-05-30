2e01eb08cfd73a865dfb5393b25d01db84bf8380
tracer-ics2017 <tracer@njuics.org>
2023-05-30 15:53:00 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  15:53:00 up 11 min,  3 users,  load average: 1.61, 1.26, 0.66 241d02083caac658271b841d222f24db606ce759

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,25 +1,52 @@
 #include "common.h"
 #include "syscall.h"
 
-int sys_none(uint32_t r){
+int sys_none()
+{
   return 1;
 }
 
-int sys_exit(uint32_t r){
+int sys_exit(uintptr_t r)
+{
   _halt(r);
 }
 
-_RegSet* do_syscall(_RegSet *r) {
+int sys_write(uintptr_t fd, uintptr_t buf, uintptr_t cnt)
+{
+  switch (fd)
+  {
+  case 1:
+  case 2:
+    for(int i=0;i<cnt;i++){
+      _putc(((char *)buf)[i]);
+    }
+    return cnt;
+  default:
+    return -1;
+  }
+
+}
+
+_RegSet *do_syscall(_RegSet *r)
+{
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
-  printf("a0=%d,a1=%d",a[0],a[1]);
-  switch (a[0]) {
-    case SYS_none: SYSCALL_ARG1(r)=sys_none(a[0]);break;
-    case SYS_exit: SYSCALL_ARG1(r)=sys_exit(a[1]);break;
-    default: panic("Unhandled syscall ID = %d", a[0]);
+  // printf("a0=%d,a1=%d",a[0],a[1]);
+  switch (a[0])
+  {
+  case SYS_none:
+    SYSCALL_ARG1(r) = sys_none();
+    break;
+  case SYS_exit:
+    SYSCALL_ARG1(r) = sys_exit(a[1]);
+    break;
+  case SYS_write:
+    SYSCALL_ARG1(r) = sys_write(a[1], a[2], a[3]);
+  default:
+    panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
