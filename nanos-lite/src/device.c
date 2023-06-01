8c5aa859dd6958a5648328dfa723a97d8428a143
tracer-ics2017 <tracer@njuics.org>
2023-06-01 13:04:20 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  13:04:20 up 20:55,  2 users,  load average: 1.97, 1.19, 1.02 643a6dca1462dfdd3522c5236d819428f3b9b7

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,6 +20,12 @@ void dispinfo_read(void *buf, off_t offset, size_t len)
 
 void fb_write(const void *buf, off_t offset, size_t len)
 {
+  int x = (offset / 4) % _screen.width;
+  int y = (offset / 4) / _screen.width;
+  int w = len / 4;
+  int h = 1;
+  _draw_rect((uint32_t *)buf, x, y, w, h);
+  return len;
 }
 
 void init_device()
@@ -29,5 +35,4 @@ void init_device()
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
   sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", _screen.width, _screen.height);
-  printf("%s", dispinfo);
 }
