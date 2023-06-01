a8190be31c89e854f10e0c6b9b417eff7fca8883
tracer-ics2017 <tracer@njuics.org>
2023-06-01 12:11:21 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  12:11:21 up 20:02,  2 users,  load average: 0.66, 0.58, 0.63 d2d13548d55219214ecb940e4a52136bed475d5

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -9,7 +9,7 @@ int main()
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-
+  printf("step 1");
   fseek(fp, 500 * 5, SEEK_SET);
   size = ftell(fp);
   printf("size:%d\n", size);
@@ -19,19 +19,19 @@ int main()
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("step 2");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i++)
   {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-
+  printf("step 3");
   for (i = 500; i < 1000; i++)
   {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("step 4");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i++)
   {
