619be00fe4c607c63379aece30d31b820f557af0
tracer-ics2017 <tracer@njuics.org>
2023-06-01 13:13:41 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  13:13:41 up 21:04,  2 users,  load average: 0.80, 0.84, 0.93 5f57b6a975dd27a33a6000a0ed27a9a698741823

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -6,13 +6,19 @@
 int main()
 {
   NDL_Bitmap *bmp = (NDL_Bitmap *)malloc(sizeof(NDL_Bitmap));
-  NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
-  assert(bmp->pixels);
-  NDL_OpenDisplay(bmp->w, bmp->h);
-  NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-  NDL_Render();
-  NDL_CloseDisplay();
   printf("done\n");
+  NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
+  printf("done\n");
+  assert(bmp->pixels);
+  printf("done\n");
+  NDL_OpenDisplay(bmp->w, bmp->h);
+  printf("done\n");
+  NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
+  printf("done\n");
+  NDL_Render();
+  printf("done\n");
+  NDL_CloseDisplay();
+
   while (1)
     ;
   return 0;
