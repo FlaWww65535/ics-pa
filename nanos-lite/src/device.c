ccdeaaa287703ec154919f67698dd95e78723b8f
tracer-ics2017 <tracer@njuics.org>
2023-06-01 13:01:16 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  13:01:16 up 20:52,  2 users,  load average: 0.60, 0.82, 0.91 fd7250b3fd647cceeac88c85ddbc60c311365ee

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,4 +29,5 @@ void init_device()
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
   sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", _screen.width, _screen.height);
+  printf("%s", dispinfo);
 }
