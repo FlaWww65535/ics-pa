5029655604a3cb09aedcdc07ceb569b7e1a80918
tracer-ics2017 <tracer@njuics.org>
2023-06-01 11:42:56 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  11:42:56 up 19:33,  2 users,  load average: 1.08, 0.83, 0.78 7647df36206895fdc91c7905d544fbde2cbf827e

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -13,137 +13,166 @@
 extern char _end;
 static intptr_t program_break = (intptr_t)&_end;
 
-int _syscall_(int type, uintptr_t a0, uintptr_t a1, uintptr_t a2){
+int _syscall_(int type, uintptr_t a0, uintptr_t a1, uintptr_t a2)
+{
   int ret = -1;
-  asm volatile("int $0x80": "=a"(ret): "a"(type), "b"(a0), "c"(a1), "d"(a2));
+  asm volatile("int $0x80"
+               : "=a"(ret)
+               : "a"(type), "b"(a0), "c"(a1), "d"(a2));
   return ret;
 }
 
-void _exit(int status) {
+void _exit(int status)
+{
   _syscall_(SYS_exit, status, 0, 0);
 }
 
-int _open(const char *path, int flags, mode_t mode) {
-  return _syscall_(SYS_open,path,flags,mode);
+int _open(const char *path, int flags, mode_t mode)
+{
+  return _syscall_(SYS_open, path, flags, mode);
 }
 
-int _write(int fd, void *buf, size_t count){
-  return _syscall_(SYS_write,fd,(uintptr_t)buf,count);
+int _write(int fd, void *buf, size_t count)
+{
+  return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
 }
 
-void *_sbrk(intptr_t increment){
-  if(_syscall_(SYS_brk,program_break+increment,0,0)==0){
+void *_sbrk(intptr_t increment)
+{
+  if (_syscall_(SYS_brk, program_break + increment, 0, 0) == 0)
+  {
     int temp = program_break;
-    program_break=program_break+increment;
-    return (void *)temp;  
+    program_break = program_break + increment;
+    return (void *)temp;
   }
   return (void *)-1;
 }
 
-int _read(int fd, void *buf, size_t count) {
-  return _syscall_(SYS_read,fd,buf,count);
+int _read(int fd, void *buf, size_t count)
+{
+  return _syscall_(SYS_read, fd, buf, count);
 }
 
-int _close(int fd) {
-  return _syscall_(SYS_close,fd,0,0);
+int _close(int fd)
+{
+  return _syscall_(SYS_close, fd, 0, 0);
 }
 
-off_t _lseek(int fd, off_t offset, int whence) {
-  return _syscall_(SYS_close,fd,offset,whence);
+off_t _lseek(int fd, off_t offset, int whence)
+{
+  return _syscall_(SYS_lseek, fd, offset, whence);
 }
 
 // The code below is not used by Nanos-lite.
 // But to pass linking, they are defined as dummy functions
 
 // not implement but used
-int _fstat(int fd, struct stat *buf) {
+int _fstat(int fd, struct stat *buf)
+{
   return 0;
 }
 
-int execve(const char *fname, char * const argv[], char *const envp[]) {
+int execve(const char *fname, char *const argv[], char *const envp[])
+{
   assert(0);
   return -1;
 }
 
-int _execve(const char *fname, char * const argv[], char *const envp[]) {
+int _execve(const char *fname, char *const argv[], char *const envp[])
+{
   return execve(fname, argv, envp);
 }
 
-int _kill(int pid, int sig) {
+int _kill(int pid, int sig)
+{
   _exit(-SYS_kill);
   return -1;
 }
 
-pid_t _getpid() {
+pid_t _getpid()
+{
   _exit(-SYS_getpid);
   return 1;
 }
 
 char **environ;
 
-time_t time(time_t *tloc) {
+time_t time(time_t *tloc)
+{
   assert(0);
   return 0;
 }
 
-int signal(int num, void *handler) {
+int signal(int num, void *handler)
+{
   assert(0);
   return -1;
 }
 
-pid_t _fork() {
+pid_t _fork()
+{
   assert(0);
   return -1;
 }
 
-int _link(const char *d, const char *n) {
+int _link(const char *d, const char *n)
+{
   assert(0);
   return -1;
 }
 
-int _unlink(const char *n) {
+int _unlink(const char *n)
+{
   assert(0);
   return -1;
 }
 
-pid_t _wait(int *status) {
+pid_t _wait(int *status)
+{
   assert(0);
   return -1;
 }
 
-clock_t _times(void *buf) {
+clock_t _times(void *buf)
+{
   assert(0);
   return 0;
 }
 
-int _gettimeofday(struct timeval *tv) {
+int _gettimeofday(struct timeval *tv)
+{
   assert(0);
   tv->tv_sec = 0;
   tv->tv_usec = 0;
   return 0;
 }
 
-int _fcntl(int fd, int cmd, ... ) {
+int _fcntl(int fd, int cmd, ...)
+{
   assert(0);
   return 0;
 }
 
-int pipe(int pipefd[2]) {
+int pipe(int pipefd[2])
+{
   assert(0);
   return 0;
 }
 
-int dup(int oldfd) {
+int dup(int oldfd)
+{
   assert(0);
   return 0;
 }
 
-int dup2(int oldfd, int newfd) {
+int dup2(int oldfd, int newfd)
+{
   assert(0);
   return 0;
 }
 
-pid_t vfork(void) {
+pid_t vfork(void)
+{
   assert(0);
   return 0;
 }
