259058b01af5b4d4e14f9236298f7cf8f336c966
tracer-ics2017 <tracer@njuics.org>
2023-06-13 22:18:16 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  22:18:16 up  7:04,  2 users,  load average: 0.47, 0.64, 0.93 36234629aeb9df6684bafb1e942eb1c6844489b6

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -3,23 +3,48 @@
 
 static void *pf = NULL;
 
-void* new_page(void) {
+void *new_page(void)
+{
   assert(pf < (void *)_heap.end);
   void *p = pf;
   pf += PGSIZE;
   return p;
 }
 
-void free_page(void *p) {
+void free_page(void *p)
+{
   panic("not implement yet");
 }
 
 /* The brk() system call handler. */
-int mm_brk(uint32_t new_brk) {
+int mm_brk(uint32_t new_brk)
+{
+  if (current->cur_brk == 0)
+  {
+    current->cur_brk = current->max_brk = new_brk;
+  }
+  else
+  {
+    if (new_brk > current->max_brk)
+    {
+      // TODO: map memory region [current->max_brk, new_brk)
+      // into address space current->as
+      for (; current->max_brk < new_brk; current->max_brk += PGSIZE)
+      {
+        void *pg = new_page();
+        _map(&current->as, current->max_brk, pg);
+      }
+      current->max_brk = new_brk;
+    }
+
+    current->cur_brk = new_brk;
+  }
+
   return 0;
 }
 
-void init_mm() {
+void init_mm()
+{
   pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
   Log("free physical pages starting from %p", pf);
 
