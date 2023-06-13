d0c6356d309c162e994136efa83519a0a4af1150
tracer-ics2017 <tracer@njuics.org>
2023-06-13 16:36:28 +0800

    >  compile 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  16:36:28 up  1:22,  2 users,  load average: 0.87, 0.65, 0.63 c3be6ea99e773bbeb05851255644db2a55126862

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -12,6 +12,7 @@ static inline uint32_t instr_fetch(vaddr_t *eip, int len)
 {
   printf("instr_fetch\n");
   uint32_t instr = vaddr_read(*eip, len);
+  printf("instr_fetch\n");
 #ifdef DEBUG
   uint8_t *p_instr = (void *)&instr;
   int i;
@@ -21,6 +22,7 @@ static inline uint32_t instr_fetch(vaddr_t *eip, int len)
   }
 #endif
   (*eip) += len;
+  printf("instr_fetch\n");
   return instr;
 }
 
