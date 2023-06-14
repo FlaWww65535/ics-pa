afbdd6b40f447fc763bad720c019e452404d0453
tracer-ics2017 <tracer@njuics.org>
2023-06-14 17:36:56 +0800

    >  run 2012235 flaw Linux archos 6.3.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 24 May 2023 17:44:00 +0000 x86_64 GNU/Linux  17:36:56 up  4:40,  2 users,  load average: 0.56, 0.65, 0.74 b914972284e03df31014ede8f1f11dcd558dade

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,8 +23,9 @@ size_t events_read(void *buf, size_t len)
   {
     if (key == _KEY_F12)
     {
-      Log("switch current");
+
       current_game = current_game % 2 + 1;
+      Log("switch current %d", current_game);
     }
     if (key & KEYDOWN_MASK)
     {
